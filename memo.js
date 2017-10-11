fa21a86cdcb811229761b3a71dbcc37114eca97a
tutu <tutugamestutubooks@gmail.com>
2017-10-11 17:12:15 +0900

    counter

--- memo.js
+++ memo.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded",function(){
   const textarea = document.getElementById("textarea")
+  const valueCount = document.getElementById("valueCount")
 
-  let theValue = textarea.value;
   
   chrome.storage.sync.get("key", function (value) {
     textarea.value = value.key;
@@ -13,7 +13,37 @@ document.addEventListener("DOMContentLoaded",function(){
     });
   }
 
-  textarea.addEventListener("keyup",saveChanges,false)
+  textarea.addEventListener("keyup", function() {
+    saveChanges();
+      let count = this.value.replace(/\r?\n/g,"").length
+      valueCount.innerHTML  = count + " 文字"
+  },false)
+
+  textarea.addEventListener("keyup",function(event){
+    console.lof(scrollHeight)
+
+    console.lof(event.target.style.scrollHeight)
+
+    console.lof()
+    
+    if(event.target.scrollHeight > event.target.offsetHeight){
+      event.target.style.height = event.target.scrollHeight + "rem";
+    } else {
+      let hight ,lineHeight;
+      while (true){
+        height = Number(event.target.style.height.split("rem")[0]);
+        lineHeight = Number(event.target.style.scrollHeight.split("rem")[0]);
+        event.target.style.height = height - lineHeight + "rem";
+        if(event.target.scrollHeight > event.target.offsetHeight){
+          event.target.style.height = event.target.scrollHeight + "rem";
+          break;
+        }        
+      }
+    }
+  })
+
+  
+
 
 })
 
