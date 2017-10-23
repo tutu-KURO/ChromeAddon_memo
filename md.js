9889bf2166bbd6cb1d85a3e09fb9e6bf9b3867ee
tutu <tutugamestutubooks@gmail.com>
2017-10-23 14:59:37 +0900

    10/23save

--- md.js
+++ md.js
@@ -13,87 +13,109 @@
 
 // })
 
-(function () {
-  'use strict';
-   //the rest of the function
-}());
 
-var GL ={};
-GL.commonmark = window.commonmark;
-GL.writer = new commonmark.HtmlRenderer({sourcepos: true});
-GL.reader = new commonmark.Parser();
-GL.inputInterval = null;
 
-// Convert Markdown to HTML
-// (1) Code Highlight (hilight.js)
-// (2) Math Fomula (mathjax.js)
-GL.convertMarkDownToHtml = function(){
-  var textarea = $("#input_area");
-  var parsed = GL.reader.parse(textarea.val());
-  // rendering
-  $("div#preview_area").html(GL.writer.render(parsed));
-  
-  // Syntax Highlight (Use "highlight.js")
-  $('pre code').each(function(i, block) {
+document.addEventListener("DOMContentLoaded", function () {
+
+  const textarea = document.getElementById("input_area");
+
+  (function () {
+    'use strict';
+    //the rest of the function
+  }());
+
+  let GL = {};
+  GL.commonmark = window.commonmark;
+  GL.writer = new commonmark.HtmlRenderer({ sourcepos: true });
+  GL.reader = new commonmark.Parser();
+  GL.inputInterval = null;
+
+  // Convert Markdown to HTML
+  // (1) Code Highlight (hilight.js)
+  // (2) Math Fomula (mathjax.js)
+  GL.convertMarkDownToHtml = function () {
+    let textarea = $("#input_area");
+    let parsed = GL.reader.parse(textarea.val());
+    // rendering
+    $("div#preview_area").html(GL.writer.render(parsed));
+
+    // Syntax Highlight (Use "highlight.js")
+    $('pre code').each(function (i, block) {
       hljs.highlightBlock(block);
-  });
-  //MathJax.Hub.Typeset(["Typeset",MathJax.Hub, "posts-preview"]);  
-};
+    });
+    //MathJax.Hub.Typeset(["Typeset",MathJax.Hub, "posts-preview"]);  
+  };
 
-// insert string at current Text Caret
-GL.insertAtCaret = function(target, str){
-    var obj = $(target);
+  // insert string at current Text Caret
+  GL.insertAtCaret = function (target, str) {
+    let obj = $(target);
     obj.focus();
     // Case of IE
-    if(navigator.userAgent.match(/MSIE/)){
-        var r = document.selection.createRange();
-        r.text = str;
-        r.select();
-    // Case of Others
-    }else{
-        var s = obj.val();
-        var p = obj.get(0).selectionStart;
-        var np = p + str.length;
-        obj.val(s.substr(0, p) + str + s.substr(p));
-        obj.get(0).setSelectionRange(np, np);
+    if (navigator.userAgent.match(/MSIE/)) {
+      let r = document.selection.createRange();
+      r.text = str;
+      r.select();
+      // Case of Others
+    } else {
+      let s = obj.val();
+      let p = obj.get(0).selectionStart;
+      let np = p + str.length;
+      obj.val(s.substr(0, p) + str + s.substr(p));
+      obj.get(0).setSelectionRange(np, np);
     }
-};
+  };
 
-$(document).ready(function(){
-  GL.convertMarkDownToHtml();
-
-  // Add event of keypress & keyup
-  $('textarea#input_area').keypress(function() {
-    if(GL.inputInterval) self.clearTimeout(GL.inputInterval);
-    GL.inputInterval = setTimeout(GL.convertMarkDownToHtml, 500);
-  });
-  $('textarea#input_area').keyup(function(e) {
-    if (e.keyCode == 46 || e.keyCode == 8){
-      if(GL.inputInterval) clearTimeout(GL.inputInterval);
-      GL.inputInterval = setTimeout(GL.convertMarkDownToHtml, 500);
-    }
-  });
-  
-  // Add event of Drag & Drop of IMAGE
-  // Change TEXTAREA Border 
-  $('textarea#input_area').on('dragover', function(e){
-    console.log("dragover");
-    $('textarea#input_area').css('border', '4px green dashed');
-  });
-  $('textarea#input_area').on('dragleave', function(e){
-    console.log("dragleave");
-    $('textarea#input_area').css('border', '4px gray solid');
-  });
-  
-  $('textarea#input_area').on('drop', function(e){
-    e.preventDefault();  // Invalid Browser Default Drag Action
-    var file = e.originalEvent.dataTransfer.files[0];
-    var imageMarkdown = "![sample-image]("+window.URL.createObjectURL(file)+")";
-
-    GL.insertAtCaret('textarea#input_area', imageMarkdown);
-    $('textarea#input_area').css('border', '4px gray solid');
-
+  $(document).ready(function () {
     GL.convertMarkDownToHtml();
+
+    // Add event of keypress & keyup
+    $('textarea#input_area').keypress(function () {
+      if (GL.inputInterval) self.clearTimeout(GL.inputInterval);
+      GL.inputInterval = setTimeout(GL.convertMarkDownToHtml, 500);
+    });
+    $('textarea#input_area').keyup(function (e) {
+      if (e.keyCode == 46 || e.keyCode == 8) {
+        if (GL.inputInterval) clearTimeout(GL.inputInterval);
+        GL.inputInterval = setTimeout(GL.convertMarkDownToHtml, 500);
+      }
+    });
+
+    // Add event of Drag & Drop of IMAGE
+    // Change TEXTAREA Border 
+    $('textarea#input_area').on('dragover', function (e) {
+      console.log("dragover");
+      $('textarea#input_area').css('border', '4px green dashed');
+    });
+    $('textarea#input_area').on('dragleave', function (e) {
+      console.log("dragleave");
+      $('textarea#input_area').css('border', '4px gray solid');
+    });
+
+    $('textarea#input_area').on('drop', function (e) {
+      e.preventDefault();  // Invalid Browser Default Drag Action
+      let file = e.originalEvent.dataTransfer.files[0];
+      let imageMarkdown = "![sample-image](" + window.URL.createObjectURL(file) + ")";
+
+      GL.insertAtCaret('textarea#input_area', imageMarkdown);
+      $('textarea#input_area').css('border', '4px gray solid');
+
+      GL.convertMarkDownToHtml();
+    });
+
   });
-  
-});
+
+  // chrome.storage.sync.get("key", function (value) {
+  //   textarea.value = value.key;
+  // });
+
+  // function saveChanges() {
+  //   let theValue = textarea.value;
+  //   chrome.storage.sync.set({'key': theValue}, function() {
+  //   });
+  // }
+
+  // textarea.addEventListener("keyup", function() {
+  //   saveChanges();
+  // })
+
+})
--- memo.js
+++ memo.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded",function(){
   const textarea = document.getElementById("textarea")
   const valueCount = document.getElementById("valueCount")
-  const md = document.getElementById("md")
+
   
   chrome.storage.sync.get("key", function (value) {
     textarea.value = value.key;
@@ -18,10 +18,6 @@ document.addEventListener("DOMContentLoaded",function(){
       let count = this.value.replace(/\s/g,"").length
       valueCount.innerHTML  = count + " 文字"
   },false)
-
-  function mdChange (){
-    location.href ="md.html"
-  }  
 
 
 })
