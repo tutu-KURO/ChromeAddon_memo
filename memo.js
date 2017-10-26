a98973d63e75a61ee839d38e4d0a7681f0e6a178
tutu <tutugamestutubooks@gmail.com>
2017-10-26 16:53:16 +0900

    searc

--- memo.js
+++ memo.js
@@ -1,23 +1,53 @@
-document.addEventListener("DOMContentLoaded",function(){
-  const textarea = document.getElementById("textarea")
-  const valueCount = document.getElementById("valueCount")
+document.addEventListener("DOMContentLoaded", function () {
+  const textarea = document.getElementById("textarea");
+  const search 　= document.getElementById("search");
+  const searchCount = document.getElementById("searchCount");
+  const valueCount = document.getElementById("valueCount");
 
   
+
+
   chrome.storage.sync.get("key", function (value) {
     textarea.value = value.key;
   });
 
   function saveChanges() {
     let theValue = textarea.value;
-    chrome.storage.sync.set({'key': theValue}, function() {
+    chrome.storage.sync.set({ 'key': theValue }, function () {
     });
   }
 
-  textarea.addEventListener("keyup", function() {
+  function textSearchCount() {
+    if (!search.value.replace(/\s/g, "")) {
+      return 0;
+    } else {
+      let theValue = textarea.value;
+      let reg = new RegExp(search.value, 'gi');
+      let count = (theValue.match(reg) || []).length;
+      
+      //console.log(theValue.match(reg))
+      // .fontcolor("red");
+
+      console.log("カウント" + count)
+      return count;
+    }
+  }
+
+  search.addEventListener("keyup", function () {
+    searchCount.innerHTML = textSearchCount() + "文字";
+    
+    
+  })
+
+
+  textarea.addEventListener("keyup", function () {
     saveChanges();
-      let count = this.value.replace(/\s/g,"").length
-      valueCount.innerHTML  = count + " 文字"
-  },false)
+    textSearchCount();
+    let count = this.value.replace(/\s/g, "").length;
+    valueCount.innerHTML = count + " 文字";
+  }, false)
+
+
 
 
 })
